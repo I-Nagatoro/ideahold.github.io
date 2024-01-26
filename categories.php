c3e30d4f6f31e890bd7494ab7d1b337bb633526e
Alex <dis606244@gmail.com>
2024-01-26 17:57:59 +0300

    init commit

---
+++ categories.php
@@ -0,0 +1,32 @@
+<?php
+// Подключение к базе данных
+$servername = "ваш_хост";
+$username = "ваше_имя_пользователя";
+$password = "ваш_пароль";
+$dbname = "ваша_база_данных";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Проверка соединения
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Запрос к базе данных для получения категорий
+$sql = "SELECT id, category_name FROM categories";
+$result = $conn->query($sql);
+
+// Создание кнопок на основе категорий
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo '<div class="item">
+                <img src="1.png" alt="" class="img">
+                <button class="btn" id="btn' . $row["id"] . '">' . $row["category_name"] . '</button>
+            </div>';
+    }
+} else {
+    echo "0 results";
+}
+
+$conn->close();
+?>
